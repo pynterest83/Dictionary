c1bfbae85f85248d98dc4933cb6c4868679b9c65
Menion <qphuc21158@gmail.com>
2023-09-16 11:49:07 +0700

    Test commit

--- src/main/java/uet/chuhuyquang/dictionary/RunMain.java
+++ src/main/java/uet/chuhuyquang/dictionary/RunMain.java
@@ -3,5 +3,6 @@ package uet.chuhuyquang.dictionary;
 public class RunMain {
     public static void main(String[] args) {
         HelloApplication.main(args);
+        System.out.println("Hello World\n");
     }
 }
