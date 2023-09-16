e94cc2a2e4b871064ab3a2d9413b98fe0619ed77
Chu Huy Quang <quangdtm2004@gmail.com>
2023-09-16 17:49:54 +0700

    Config

--- src/main/RunMain.java
+++ src/main/RunMain.java
@@ -1,9 +1,8 @@
 package main;
 
-import main.HelloApplication;
-
 public class RunMain {
     public static void main(String[] args) {
         HelloApplication.main(args);
+        System.out.println("Hello, world!");
     }
 }
