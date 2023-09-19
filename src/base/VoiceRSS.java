842cfd080d6317e21fb9fc457fbd97579f1d522e
Chu Huy Quang <quangdtm2004@gmail.com>
2023-09-19 09:53:01 +0700

    test audio

---
+++ src/base/VoiceRSS.java
@@ -0,0 +1,40 @@
+package base;
+
+import com.voicerss.tts.AudioFormat;
+import com.voicerss.tts.VoiceParameters;
+import com.voicerss.tts.VoiceProvider;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class VoiceRSS {
+    private static final String API_KEY = "4aceebbbab2946d5b434b3bb810bd2eb";
+    private static final String AUDIO_PATH = "src/resources/audio.wav";
+
+    public static String voiceNameUS;
+    public static String voiceNameUK;
+    public static String language = "en-gb";
+    public static String Name = "Linda";
+    public static double speed = 1;
+    public static void speakWord(String word) throws Exception {
+        VoiceProvider tts = new VoiceProvider(API_KEY);
+        VoiceParameters params = new VoiceParameters(word, AudioFormat.Format_44KHZ.AF_44khz_16bit_stereo);
+        params.setBase64(false);
+        params.setLanguage(language);
+        params.setVoice(Name);
+        params.setRate((int) Math.round(-2.9936 * speed * speed + 15.2942 * speed - 12.7612));
+
+        byte[] voice = tts.speech(params);
+
+        FileOutputStream fos = new FileOutputStream(AUDIO_PATH);
+        fos.write(voice, 0, voice.length);
+        fos.flush();
+        fos.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        speakWord("good morning");
+    }
+}
--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module base {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires voicerss.tts;
 
     opens base to javafx.fxml;
     exports base;
