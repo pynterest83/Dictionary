8ba857426c9994a525b2777567e91347fe193d8c
VaguePasta <qphuc21158@gmail.com>
2023-09-29 11:32:57 +0700

    Remove redundant files

--- src/module-info.java
+++ src/module-info.java
@@ -2,7 +2,6 @@ module main {
         requires javafx.controls;
         requires javafx.fxml;
         requires javafx.media;
-        requires voicerss.tts;
         requires org.controlsfx.controls;
         requires javafx.web;
         requires AnimateFX;
