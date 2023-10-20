d9e61e9769bce5a1986c7f448f8a9c88b940ce14
VaguePasta <qphuc21158@gmail.com>
2023-10-20 21:37:05 +0700

    Test loading animation

--- src/main/GGTranslateController.java
+++ src/main/GGTranslateController.java
@@ -1,17 +1,23 @@
 package main;
 
 import base.TranslateAPI;
-import javafx.event.ActionEvent;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import org.controlsfx.control.SearchableComboBox;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
+import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class GGTranslateController extends MainController{
     private static final String PATH = "src/resources/Spelling.txt";
@@ -30,7 +36,16 @@ public class GGTranslateController extends MainController{
     @FXML
     private TextArea antonyms;
     @FXML
-    private void initialize() throws IOException {
+    private Button symButton;
+    @FXML
+    private Button antButton;
+    @FXML
+    private ImageView loading = new ImageView(Paths.get("src/style/media/loading.gif").toUri().toURL().toString());
+    public GGTranslateController() throws MalformedURLException {
+    }
+
+    @FXML
+    private void initialize() {
         PrepareMenu(true);
         for (HashMap.Entry<String, String> entry : TranslateAPI.langMap.entrySet()) {
             SourceLang.getItems().add(entry.getKey());
@@ -39,15 +54,15 @@ public class GGTranslateController extends MainController{
     }
 
     @FXML
-    public void SelectSourceLang(ActionEvent actionEvent) {
+    public void SelectSourceLang() {
         sourceLangCode = TranslateAPI.langMap.get(SourceLang.getValue());
     }
     @FXML
-    public void SelectTargetLang(ActionEvent actionEvent) {
+    public void SelectTargetLang() {
         targetLangCode = TranslateAPI.langMap.get(TargetLang.getValue());
     }
     @FXML
-    public void swap(ActionEvent actionEvent) {
+    public void swap() {
         String temp = SourceLang.getValue();
         SourceLang.setValue(TargetLang.getValue());
         TargetLang.setValue(temp);
@@ -57,9 +72,8 @@ public class GGTranslateController extends MainController{
         output.setText(tmp);
     }
     @FXML
-    public void translate(ActionEvent actionEvent) {
-        try {
-            if (input.getText().equals("")) {
+    public void translate() {
+            if (input.getText().isEmpty()) {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Error");
                 alert.setHeaderText("Please enter a word");
@@ -67,7 +81,7 @@ public class GGTranslateController extends MainController{
                 alert.showAndWait();
                 return;
             }
-            if (targetLangCode == "") {
+            if (Objects.equals(targetLangCode, "")) {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Error");
                 alert.setHeaderText("Please select a target language");
@@ -75,15 +89,32 @@ public class GGTranslateController extends MainController{
                 alert.showAndWait();
                 return;
             }
-            output.setText(TranslateAPI.googleTranslate(sourceLangCode, targetLangCode, input.getText()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
+            String translate = input.getText();
+            output.clear();
+            new Thread(()-> {
+                AnchorPane parent = (AnchorPane) output.getParent();
+                loading.setFitHeight(60);
+                loading.setFitWidth(60);
+                loading.setLayoutY(0);
+                ImageView finalLoading = loading;
+                Platform.runLater(()-> {
+                    parent.getChildren().add(finalLoading);
+                    input.setDisable(true);
+                });
+                try {
+                    output.setText(TranslateAPI.googleTranslate(sourceLangCode, targetLangCode, translate));
+                    Platform.runLater(()-> {
+                        parent.getChildren().remove(loading);
+                        input.setDisable(false);
+                    });
+                } catch (IOException | URISyntaxException e) {
+                    parent.getChildren().remove(loading);
+                    input.setDisable(false);
+                }
+            }).start();
     }
 
-    public void onClickSpeak1(ActionEvent actionEvent) throws IOException, URISyntaxException {
+    public void onClickSpeak1() throws IOException, URISyntaxException {
         if (SourceLang.getValue() == null) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Error");
@@ -92,7 +123,7 @@ public class GGTranslateController extends MainController{
             alert.showAndWait();
             return;
         }
-        if (input.getText().equals("")) {
+        if (input.getText().isEmpty()) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Error");
             alert.setHeaderText("Please enter a word");
@@ -103,7 +134,7 @@ public class GGTranslateController extends MainController{
         TranslateAPI.speakAudio(input.getText(),SourceLang.getValue());
     }
 
-    public void onClickSpeak2(ActionEvent actionEvent) throws IOException, URISyntaxException {
+    public void onClickSpeak2() throws IOException, URISyntaxException {
         if (TargetLang.getValue() == null) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Error");
@@ -112,7 +143,7 @@ public class GGTranslateController extends MainController{
             alert.showAndWait();
             return;
         }
-        if (output.getText().equals("")) {
+        if (output.getText().isEmpty()) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Error");
             alert.setHeaderText("Please enter a word");
@@ -123,9 +154,10 @@ public class GGTranslateController extends MainController{
         TranslateAPI.speakAudio(output.getText(),TargetLang.getValue());
     }
 
-    public void onClickSynonyms(ActionEvent actionEvent) throws URISyntaxException, IOException, ParseException {
-        String word = "";
-        if (sourceLangCode.equals("en") || (sourceLangCode.equals(""))) {
+    public void onClickSynonyms() {
+        synonyms.setText("");
+        String word;
+        if (sourceLangCode.equals("en") || (sourceLangCode.isEmpty())) {
             word = input.getText();
         } else if (targetLangCode.equals("en")) {
             word = output.getText();
@@ -138,21 +170,40 @@ public class GGTranslateController extends MainController{
             alert.showAndWait();
             return;
         }
-        String[] synonymsList = TranslateAPI.theSaurus(word, "syn");
-        if (synonymsList == null) {
-            synonyms.setText("No synonyms found");
-            return;
-        }
-        String synonymsString = "";
-        for (int i = 0; i < synonymsList.length; i++) {
-            synonymsString += synonymsList[i] + "\n";
-        }
-        synonyms.setText(synonymsString);
+        new Thread(() -> {
+            loading.setFitWidth(60);
+            loading.setFitHeight(60);
+            loading.setLayoutY(180);
+            AnchorPane parent = (AnchorPane) synonyms.getParent();
+            Platform.runLater(() -> {
+                parent.getChildren().add(loading);
+                symButton.setDisable(true);
+            });
+            try {
+                String[] synonymsList = TranslateAPI.theSaurus(word, "syn");
+                if (synonymsList == null) {
+                    synonyms.setText("No synonyms found");
+                }
+                else {
+                    StringBuilder synonymsString = new StringBuilder();
+                    for (String s : synonymsList) {
+                        synonymsString.append(s).append("\n");
+                    }
+                    synonyms.setText(synonymsString.toString());
+                }
+            } catch (URISyntaxException | ParseException | IOException e) {
+                throw new RuntimeException(e);
+            }
+            Platform.runLater(() -> {
+                parent.getChildren().remove(loading);
+                symButton.setDisable(false);
+            });
+        }).start();
     }
-
-    public void onClickAntonyms(ActionEvent actionEvent) throws URISyntaxException, IOException, ParseException {
-        String word = "";
-        if (sourceLangCode.equals("en") || (sourceLangCode.equals(""))) {
+    public void onClickAntonyms() {
+        antonyms.setText("");
+        String word;
+        if (sourceLangCode.equals("en") || (sourceLangCode.isEmpty())) {
             word = input.getText();
         } else if (targetLangCode.equals("en")) {
             word = output.getText();
@@ -165,15 +216,35 @@ public class GGTranslateController extends MainController{
             alert.showAndWait();
             return;
         }
-        String[] antonymsList = TranslateAPI.theSaurus(word, "ant");
-        if (antonymsList == null) {
-            antonyms.setText("No antonyms found");
-            return;
-        }
-        String antonymsString = "";
-        for (int i = 0; i < antonymsList.length; i++) {
-            antonymsString += antonymsList[i] + "\n";
-        }
-        antonyms.setText(antonymsString);
+        new Thread(() -> {
+            loading.setFitWidth(60);
+            loading.setFitHeight(60);
+            loading.setLayoutY(180);
+            AnchorPane parent = (AnchorPane) antonyms.getParent();
+            Platform.runLater(() -> {
+                parent.getChildren().add(loading);
+
+                antButton.setDisable(true);
+            });
+            try {
+                String[] antonymsList = TranslateAPI.theSaurus(word, "ant");
+                if (antonymsList == null) {
+                    antonyms.setText("No antonyms found");
+                }
+                else {
+                    StringBuilder antonymsString = new StringBuilder();
+                    for (String s : antonymsList) {
+                        antonymsString.append(s).append("\n");
+                    }
+                    antonyms.setText(antonymsString.toString());
+                }
+            } catch (URISyntaxException | ParseException | IOException e) {
+                throw new RuntimeException(e);
+            }
+            Platform.runLater(() -> {
+                parent.getChildren().remove(loading);
+                antButton.setDisable(false);
+            });
+        }).start();
     }
 }
