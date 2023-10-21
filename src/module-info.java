09ee0b042cd3702a3afcbadb2000775df51e1bc6
VaguePasta <qphuc21158@gmail.com>
2023-10-21 12:27:56 +0700

    Update javafx version

--- src/module-info.java
+++ src/module-info.java
@@ -5,8 +5,8 @@ module main {
         requires org.controlsfx.controls;
         requires javafx.web;
         requires AnimateFX;
-    requires json.simple;
-    opens base to javafx.fxml;
+        requires json.simple;
+        opens base to javafx.fxml;
         exports base;
         exports main;
         opens main to javafx.fxml;
