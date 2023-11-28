fee8109a95637dc6e8015b987970e4371b5596e7
Chu Huy Quang <quangdtm2004@gmail.com>
2023-11-28 13:07:48 +0700

    test

--- src/main/SettingController.java
+++ src/main/SettingController.java
@@ -1,10 +1,15 @@
 package main;
 
+import base.Dictionary;
+import base.DictionaryManager;
 import controls.GeneralControls;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Slider;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
+
+import java.io.IOException;
 
 public class SettingController extends GeneralControls {
     @FXML
@@ -14,8 +19,17 @@ public class SettingController extends GeneralControls {
     @FXML
     private Slider SpeakVolumeSlider;
     @FXML
+    private Button AvaPane;
+    @FXML
+    private TextField NamePane;
+    @FXML
+    private TextField GenderPane;
+    @FXML
+    private Button saveButton;
+    @FXML
     private void initialize() {
         loadOtherScences();
+        setUp();
         PrepareMenu();
         SpeakVolumeSlider.valueProperty().addListener((ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) -> {
             try {
@@ -28,5 +42,52 @@ public class SettingController extends GeneralControls {
     @FXML
     private void MouseClick() {
         if (menuOpen) HideMenuBar();
+    }
+    @FXML
+    public void onClickUpdateUser(ActionEvent actionEvent) throws IOException {
+        if (NamePane.getText().equals("")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Name cannot be empty");
+            alert.showAndWait();
+            return;
+        }
+        Dictionary.user.username = NamePane.getText();
+        if (!GenderPane.getText().equalsIgnoreCase("Male") || !GenderPane.getText().equalsIgnoreCase("Female")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Please enter valid gender: Male or Female");
+        }
+        if (GenderPane.getText().equalsIgnoreCase("Male")) Dictionary.user.gender = 1;
+        else if (GenderPane.getText().equalsIgnoreCase("Female")) Dictionary.user.gender = 0;
+        if (Dictionary.user.streak == null) {
+            Dictionary.user.streak = java.time.LocalDate.now().toString();
+            Dictionary.user.streakOn = true;
+        }
+
+        setUp();
+        loadOtherScences();
+        RunApplication.LoadScenes();
+        DictionaryManager.updateUser();
+    }
+
+    @FXML
+    private void setUp() {
+        if (Dictionary.user.username != null) NamePane.setText(Dictionary.user.username);
+        else NamePane.setText("User");
+
+        if (Dictionary.user.gender == 0) {
+            AvaPane.getStyleClass().clear();
+            AvaPane.getStyleClass().add("woman-avatar");
+            GenderPane.setText("Female");
+        }
+        else if (Dictionary.user.gender == 1) {
+            AvaPane.getStyleClass().clear();
+            AvaPane.getStyleClass().add("man-avatar");
+            GenderPane.setText("Male");
+        }
+        else {
+            GenderPane.setText("Unknown");
+        }
     }
 }
